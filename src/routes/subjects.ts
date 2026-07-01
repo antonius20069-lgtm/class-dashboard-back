c35ab86f8d96ad147b8db1a6743cfcd027749cce
antonius <anotnius20069@gmail.com>
2026-07-01 19:13:24 +0300

    second

--- src/routes/subjects.ts
+++ src/routes/subjects.ts
@@ -9,8 +9,8 @@ router.get('/', async (req, res) => {
     try {
 const {search, department ,page=1 ,limit = 10 } = req.query;
 
-        const currentPage =  Math.max(1, +page);
-        const LimitPerPage = Math.max(1, +limit);
+        const currentPage =  Math.max(1,parseInt(String(page),10) ||1);
+        const LimitPerPage = Math.min(Math.max(1,parseInt(String(limit),10) ||1),100)
         const offset = (currentPage - 1) * +limit;
 
 const filterCoditions = []
